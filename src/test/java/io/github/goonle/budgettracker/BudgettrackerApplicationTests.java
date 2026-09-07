0231cbc3b573f6cd6383902ebb9ddeeea96c2cd5
Junhyung Huh <junhuh741@gmail.com>
2026-09-07 22:11:34 +1200

    chore: initialize Spring Boot project
     - Java 25, Spring Boot 4.1.1
     - Dependencies: Web, Data JPA, PostgreSQL, Validation, Lombok,
       Configuration Processor, SpringDoc OpenAPI, Actuator, DevTools
     - Packaging: JAR

---
+++ src/test/java/io/github/goonle/budgettracker/BudgettrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.goonle.budgettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgettrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
